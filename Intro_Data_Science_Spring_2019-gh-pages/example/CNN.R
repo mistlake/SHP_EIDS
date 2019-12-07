7b9681db679503921939fff37b35e1d89cf8e2b2
Owen Ward <33092609+OwenWard@users.noreply.github.com>
2019-12-07 09:44:27 -0500

    Add files via upload

--- Intro_Data_Science_Spring_2019-gh-pages/example/CNN.R
+++ Intro_Data_Science_Spring_2019-gh-pages/example/CNN.R
@@ -14,6 +14,19 @@ x_test <- mnist$test$x
 y_test <- mnist$test$y
 
 
+x_train <- mnist$train$x
+y_train <- mnist$train$y
+par(mfcol=c(6,6))
+par(mar=c(0, 0, 3, 0), xaxs='i', yaxs='i')
+for (idx in 1:36) { 
+  im <- x_train[idx,,]
+  im <- t(apply(im, 2, rev)) 
+  image(1:28, 1:28, im, col=gray((0:255)/255), 
+        xaxt='n', main=paste(y_train[idx]))
+}
+
+
+
 dim(x_train) <- c(nrow(x_train), 784)
 dim(x_test) <- c(nrow(x_test), 784)
 # rescale
@@ -32,6 +45,7 @@ model %>%
   layer_dense(units = 128, activation = "relu") %>%
   layer_dropout(rate = 0.3) %>%
   layer_dense(units = 10, activation = "softmax")
+
 
 
 summary(model)
@@ -58,3 +72,56 @@ model %>% evaluate(x_test, y_test,verbose = 0)
 
 
 model %>% predict_classes(x_test)
+
+
+## Conv Net
+batch_size <- 128
+num_classes <- 10
+epochs <- 12
+
+img_rows <- 28
+img_cols <- 28
+
+
+x_train <- array_reshape(x_train, c(nrow(x_train), img_rows, img_cols, 1))
+x_test <- array_reshape(x_test, c(nrow(x_test), img_rows, img_cols, 1))
+input_shape <- c(img_rows, img_cols, 1)
+
+
+model <- keras_model_sequential() %>%
+  layer_conv_2d(filters = 32, kernel_size = c(3,3), activation = 'relu',
+                input_shape = input_shape) %>% 
+  layer_conv_2d(filters = 64, kernel_size = c(3,3), activation = 'relu') %>% 
+  layer_max_pooling_2d(pool_size = c(2, 2)) %>% 
+  layer_dropout(rate = 0.25) %>% 
+  layer_flatten() %>% 
+  layer_dense(units = 128, activation = 'relu') %>% 
+  layer_dropout(rate = 0.5) %>% 
+  layer_dense(units = num_classes, activation = 'softmax')
+
+# Compile model
+model %>% compile(
+  loss = loss_categorical_crossentropy,
+  optimizer = optimizer_adadelta(),
+  metrics = c('accuracy')
+)
+
+# Train model
+model %>% fit(
+  x_train, y_train,
+  batch_size = batch_size,
+  epochs = epochs,
+  validation_split = 0.2
+)
+
+
+
+
+scores <- model %>% evaluate(
+  x_test, y_test, verbose = 0
+)
+
+# Output metrics
+cat('Test loss:', scores[[1]], '\n')
+cat('Test accuracy:', scores[[2]], '\n')
+
